5c139750477c1a170104036f16bf1e0263e5527d
Rajiv Gadda <rajiv.gadda@docusign.com>
2020-05-24 13:47:59 -0700

    ichimoku strat

--- strategy/shortTerm.js
+++ strategy/shortTerm.js
@@ -12,6 +12,44 @@ const alpaca = new Alpaca({
     paper: config.PAPER
 });
 
+const getIchimokuSignals = (input) => {
+    const ichimoku = new TI.IchimokuCloud({
+        ...input, conversionPeriod: 9,
+        basePeriod: 26,
+        spanPeriod: 52,
+        displacement: 26
+    }).getResult();
+    console.log(ichimoku[ichimoku.length - 1], input.close[input.close.length - 1])
+    const price = input.close[input.close.length - 1];
+    const prevPrice = input.close[input.close.length - 2];
+    const latestIchimokuValues = ichimoku[ichimoku.length - 1];
+    const previousIchimokuValues = ichimoku[ichimoku.length - 2];
+
+    // latest constants
+    const isPriceAboveKumoCloud = price > latestIchimokuValues.spanA && price > latestIchimokuValues.spanB;
+    const isPriceBelowKumoCloud = price < latestIchimokuValues.spanA && price < latestIchimokuValues.spanB;
+    const isPriceAboveConversionLine = price > latestIchimokuValues.conversion;
+    const isPriceAboveBaseLine = price > latestIchimokuValues.base;
+
+    // previous constants
+    const isPrevPriceAboveKumoCloud = prevPrice > previousIchimokuValues.spanA && prevPrice > previousIchimokuValues.spanB;
+    const isPrevPriceAboveConversionLine = prevPrice > previousIchimokuValues.conversion;
+    const isPrevPriceAboveBaseLine = prevPrice > previousIchimokuValues.base;
+
+    if (isPriceAboveKumoCloud) {
+        if (isPrevPriceAboveBaseLine > isPrevPriceAboveConversionLine && isPriceAboveBaseLine < isPriceAboveConversionLine) {
+            return 'golong';
+        }
+    }
+
+    if (isPriceBelowKumoCloud) {
+        if (isPrevPriceAboveBaseLine < isPrevPriceAboveConversionLine && isPriceAboveBaseLine > isPriceAboveConversionLine) {
+            return 'goshort';
+        }
+    }
+    return 'wait';
+}
+
 const buySellSignal = (data) => {
     const input = _.reduce(data, (acc, candle) => {
         return {
@@ -30,77 +68,83 @@ const buySellSignal = (data) => {
         volume: [],
         timestamp: []
     });
-    const macd = new TI.MACD({
-        values: input.close,
-        fastPeriod: 36,
-        slowPeriod: 78,
-        signalPeriod: 27
-    }).getResult()
-    const heiknashi = new TI.heikinashi(input)
-    const mfi = new TI.MFI({ ...heiknashi, period: 42 }).getResult();
-    const cci = new TI.CCI({ ...heiknashi, period: 60 }).getResult();
+
+    return getIchimokuSignals(input);
+
+    // const macd = new TI.MACD({
+    //     values: input.close,
+    //     fastPeriod: 36,
+    //     slowPeriod: 78,
+    //     signalPeriod: 27
+    // }).getResult()
+    // const heiknashi = new TI.heikinashi(input)
+    // const mfi = new TI.MFI({ ...heiknashi, period: 42 }).getResult();
+    // const cci = new TI.CCI({ ...heiknashi, period: 60 }).getResult();
 
 
-    const currentDataSet = macd[macd.length - 1];
-    const previousDataSet = macd[macd.length - 2];
 
-    if (currentDataSet.signal > 0.2 && currentDataSet.histogram < previousDataSet.histogram && mfi[mfi.length - 1] > 60 && cci[cci.length - 1] > 50) {
-        console.log(`currentDataSet.signal > 0.2: ${currentDataSet.signal} > 0.2 && 
-        currentDataSet.histogram < previousDataSet.histogram: ${currentDataSet.histogram} < ${previousDataSet.histogram}&& 
-        mfi[mfi.length - 1] > 60: ${mfi[mfi.length - 1]} > 60 && 
-        cci[cci.length - 1] > 50: ${cci[cci.length - 1]} > 50
-        #####
-        Hence go short
-        #####`);
-        return 'goshort'
-    }
 
-    if (currentDataSet.signal < -0.2 && currentDataSet.histogram > previousDataSet.histogram && mfi[mfi.length - 1] < 40 && cci[cci.length - 1] < -50) {
-        console.log(`currentDataSet.signal < -0.2: ${currentDataSet.signal} < -0.2 && 
-        currentDataSet.histogram > previousDataSet.histogram: ${currentDataSet.histogram} > ${previousDataSet.histogram}&& 
-        mfi[mfi.length - 1] < 40: ${mfi[mfi.length - 1]} < 40 && 
-        cci[cci.length - 1] < -50: ${cci[cci.length - 1]} < -50
-        #####
-        Hence go long
-        #####`);
-        return 'golong'
-    }
-    return 'wait'
+
+    // const currentDataSet = macd[macd.length - 1];
+    // const previousDataSet = macd[macd.length - 2];
+
+    // if (currentDataSet.signal > 0.2 && currentDataSet.histogram < previousDataSet.histogram && mfi[mfi.length - 1] > 60 && cci[cci.length - 1] > 50) {
+    //     console.log(`currentDataSet.signal > 0.2: ${currentDataSet.signal} > 0.2 && 
+    //     currentDataSet.histogram < previousDataSet.histogram: ${currentDataSet.histogram} < ${previousDataSet.histogram}&& 
+    //     mfi[mfi.length - 1] > 60: ${mfi[mfi.length - 1]} > 60 && 
+    //     cci[cci.length - 1] > 50: ${cci[cci.length - 1]} > 50
+    //     #####
+    //     Hence go short
+    //     #####`);
+    //     return 'goshort'
+    // }
+
+    // if (currentDataSet.signal < -0.2 && currentDataSet.histogram > previousDataSet.histogram && mfi[mfi.length - 1] < 40 && cci[cci.length - 1] < -50) {
+    //     console.log(`currentDataSet.signal < -0.2: ${currentDataSet.signal} < -0.2 && 
+    //     currentDataSet.histogram > previousDataSet.histogram: ${currentDataSet.histogram} > ${previousDataSet.histogram}&& 
+    //     mfi[mfi.length - 1] < 40: ${mfi[mfi.length - 1]} < 40 && 
+    //     cci[cci.length - 1] < -50: ${cci[cci.length - 1]} < -50
+    //     #####
+    //     Hence go long
+    //     #####`);
+    //     return 'golong'
+    // }
+    // return 'wait'
 }
 
 async function actOnSignal(signal, symbol, qty, price, target, side = false) {
     console.log(`Line 57: Signal for ${symbol}: ${signal}`)
     switch (signal) {
         case "goshort":
-            // if (side !== "short") {
-            //     console.log(`Line 66(${symbol}): close 1, ${side}, ${signal}`)
-            //     await alpaca.closePosition(symbol)
-            //         .then(async (resp) => {
-            //             console.log(`Closed your ${side} position in ${symbol}`);
-            //             console.log('placing short order');
-            //             setTimeout(() => { helperFunctions.submitOrder(qty, symbol, "sell", price, target, true); }, 500);
-            //         }).catch(async (err) => {
-            //             await helperFunctions.submitOrder(qty, symbol, "sell", price, target);
-            //         });
-            // } else {
-            //     !side ? console.log(`Wait for the right trade in ${symbol}`) : console.log(`Hold your ${side} position in ${symbol}`);
-            // } 
-            await helperFunctions.submitOrder(qty, symbol, "sell", price, target);
+            if (side !== "short") {
+                console.log(`Line 66(${symbol}): close 1, ${side}, ${signal}`)
+                await alpaca.closePosition(symbol)
+                    .then(async (resp) => {
+                        console.log(`Closed your ${side} position in ${symbol}`);
+                        console.log('placing short order');
+                        setTimeout(() => { helperFunctions.submitOrder(qty, symbol, "sell", price, target, true); }, 500);
+                    }).catch(async (err) => {
+                        await helperFunctions.submitOrder(qty, symbol, "sell", price, target);
+                    });
+            } else {
+                !side ? console.log(`Wait for the right trade in ${symbol}`) : console.log(`Hold your ${side} position in ${symbol}`);
+            }
+            // await helperFunctions.submitOrder(qty, symbol, "sell", price, target);
             break;
         case "golong":
-            // if (side !== "long") {
-            //     console.log(`Line 81(${symbol}): close 2, ${side}, ${signal}`)
-            //     await alpaca.closePosition(symbol).then(async (resp) => {
-            //         console.log(`Closed your ${side} position in ${symbol}`);
-            //         console.log(`placing long order`);
-            //         setTimeout(() => { helperFunctions.submitOrder(qty, symbol, "buy", price, target, true); }, 500);
-            //     }).catch(async (err) => {
-            //         await helperFunctions.submitOrder(qty, symbol, "buy", price, target);
-            //     });
-            // } else {
-            //     !side ? console.log(`Wait for the right trade in ${symbol}`) : console.log(`Hold your ${side} position in ${symbol}`);
-            // }
-            helperFunctions.submitOrder(qty, symbol, "buy", price, target);
+            if (side !== "long") {
+                console.log(`Line 81(${symbol}): close 2, ${side}, ${signal}`)
+                await alpaca.closePosition(symbol).then(async (resp) => {
+                    console.log(`Closed your ${side} position in ${symbol}`);
+                    console.log(`placing long order`);
+                    setTimeout(() => { helperFunctions.submitOrder(qty, symbol, "buy", price, target, true); }, 500);
+                }).catch(async (err) => {
+                    await helperFunctions.submitOrder(qty, symbol, "buy", price, target);
+                });
+            } else {
+                !side ? console.log(`Wait for the right trade in ${symbol}`) : console.log(`Hold your ${side} position in ${symbol}`);
+            }
+            // helperFunctions.submitOrder(qty, symbol, "buy", price, target);
             break;
 
         case "closelong":
@@ -135,34 +179,22 @@ const run = async (skipClosing = false) => {
     // const openOrders = await alpaca.getOrders({ status: 'open' });
     _.forEach(_.keys(config.tradeableAssets), async (symbol) => {
         let dataset = await helperFunctions.getData(symbol, config.tradeableAssets[symbol].minutes);
-        if (parseInt(dataset.results[dataset.results.length - 1].diff) > config.tradeableAssets[symbol].minutes + 1) {
-            console.log(`No data for ${symbol}`)
-            return;
-        }
+        // if (parseInt(dataset.results[dataset.results.length - 1].diff) > config.tradeableAssets[symbol].minutes + 1) {
+        //     console.log(`No data for ${symbol}`)
+        //     return;
+        // }
         const qty = config.tradeableAssets[symbol].qty;
         const signal = buySellSignal(dataset.results);
         const price = dataset.results[dataset.results.length - 1].c
         console.log(`****** Signal: ${signal}`);
-        if (signal === "wait") {
-            return;
-        } else {
-            await actOnSignal(signal, symbol, qty, price, config.tradeableAssets[symbol].target);
-        }
-        // alpaca.getPosition(symbol).then(async (position) => {
-        //     if (Number(position.unrealized_pl) >= parseInt(config.tradeableAssets[symbol].target * config.tradeableAssets[symbol].qty)) {
-        //         console.log(`Line 133(${symbol}): close 4, ${position.side}, ${signal}`)
-        //         console.log("closing position as target hit", position.unrealized_pl)
-        //         return alpaca.closePosition(position.symbol)
-        //     }
-        //     console.log(`Line 139(${symbol}): act 1, ${signal}`)
-        //     await actOnSignal(signal, symbol, qty, price, config.tradeableAssets[symbol].target, position.side);
-        // }).catch(async (err) => {
-        //     console.log(err.error)
-        //     console.log(`Line 143(${symbol}): act 2, ${signal}`)
-        //     if (signal !== "wait") {
-        //         await actOnSignal(signal, symbol, qty, price, config.tradeableAssets[symbol].target);
-        //     }
-        // })
+        alpaca.getPosition(symbol).then(async (position) => {
+            await actOnSignal(signal, symbol, qty, price, config.tradeableAssets[symbol].target, position.side);
+        }).catch(async (err) => {
+            console.log(err.error)
+            if (signal !== "wait") {
+                await actOnSignal(signal, symbol, qty, price, config.tradeableAssets[symbol].target);
+            }
+        })
     })
 }
 
@@ -180,5 +212,5 @@ module.exports = {
     test: test,
     run: run
 }
-// run(true)
+run(true)
 // test()
